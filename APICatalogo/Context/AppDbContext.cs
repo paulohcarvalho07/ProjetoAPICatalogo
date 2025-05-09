5dd34f5d30e4994a71c842fc0fe494d06b2e3dbb
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-05-09 16:08:30 -0300

    Criando a classe de contexto do EF Core

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {   
+    }
+
+    DbSet<Produto>? Produtos { get; set; }
+    DbSet<Categoria>? Categorias { get; set; }
+}
