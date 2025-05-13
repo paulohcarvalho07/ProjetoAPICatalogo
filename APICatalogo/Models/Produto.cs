6a11f72b193070cb501e0fed823daa2e4a8ee4c1
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-05-13 17:46:35 -0300

    Validacao Personalizada - I

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -12,6 +13,7 @@ public class Produto
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
+    [PrimeiraLetraMaisucula]
     public string? Nome { get; set; }
 
     [Required]
