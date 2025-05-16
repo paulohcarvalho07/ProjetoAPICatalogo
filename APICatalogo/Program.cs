f162486ce09f89f4c8ef83b3895520d0e8183cf0
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-05-16 10:43:15 -0300

    Implementando o Repositorio Generico III

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -42,6 +42,7 @@ builder.Services.AddTransient<IMeuServico, MeuServico>();
 builder.Services.AddScoped<ApiLoggingFilter>();
 builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Logging.AddProvider(new CustomerLoggerProvider(new CustomerLoggerProviderConfiguration
 {
--- APICatalogo/Repositories/ICategoriaRepository.cs
+++ APICatalogo/Repositories/ICategoriaRepository.cs
@@ -3,12 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalogo.Repositories;
 
-public interface ICategoriaRepository
+public interface ICategoriaRepository : IRepository<Categoria>
 {
-    IEnumerable<Categoria> GetCategoriasProdutos();
-    IEnumerable<Categoria> GetCategorias();
-    Categoria GetCategoria(int id);
-    Categoria Create(Categoria categoria);
-    Categoria Update(Categoria categoria);
-    Categoria Delete(int id);
+
 }
