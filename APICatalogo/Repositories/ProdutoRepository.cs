aae3454e690130439d6de192fdbc668b967095c7
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-05-16 17:21:54 -0300

    Ajustando os controladores II

--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -12,6 +12,6 @@ public class ProdutoRepository : Repository<Produto>, IProdutoRepository
 
     public IEnumerable<Produto> GetProdutosPorCategoria(int id)
     {
-        return GetAll().Where(p => p.ProdutoId == id);
+        return GetAll().Where(c => c.CategoriaId == id);
     }
 }
