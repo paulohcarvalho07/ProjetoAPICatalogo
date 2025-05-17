63f01f5bbbcc645d495b56f67a4cb385a37c6ea0
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-05-17 18:28:07 -0300

    Implementando o padrao Unit Of Work II

---
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using APICatalogo.Context;
+using System.Runtime.InteropServices;
+
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IProdutoRepository? _produtoRepo;
+    private ICategoriaRepository? _categoriaRepo;
+    
+    public AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IProdutoRepository ProdutoRepository
+    {
+        get
+        {
+            return _produtoRepo = _produtoRepo ?? new ProdutoRepository(_context);
+            //if(_produtoRepo == null)
+            //{
+            //    return new ProdutoRepository(_context);
+            //}
+            //return _produtoRepo;
+        }
+    }
+
+    public ICategoriaRepository CategoriaRepository
+    {
+        get
+        {
+            return _categoriaRepo = _categoriaRepo ?? new CategoriaRepository(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
