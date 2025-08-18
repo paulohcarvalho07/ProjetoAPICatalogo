b31140f58e96d099f07ca6beaf2b1454ff588d91
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-08-18 16:44:04 -0300

    Implementando a autenticação JWT Bearer - III

---
+++ APICatalogo/DTOs/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace APICatalogo.DTOs;
+
+public class TokenModel
+{
+    public string? AcessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
