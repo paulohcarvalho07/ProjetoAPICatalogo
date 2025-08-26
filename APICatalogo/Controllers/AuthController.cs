ec978534d8ff27cd1f8e253d0d502a0c78dea386
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-08-26 10:13:13 -0300

    Autorização baseada em políticas com JWT - Adicionando um usuário ao perfil

--- APICatalogo/Controllers/AuthController.cs
+++ APICatalogo/Controllers/AuthController.cs
@@ -63,6 +63,33 @@ public class AuthController : ControllerBase
     }
 
     [HttpPost]
+    [Route("AddUserToRole")]
+    public async Task<IActionResult> AddUserToRole(string email, string roleName)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+
+        if (user != null)
+        {
+            var result = await _userManager.AddToRoleAsync(user, roleName);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation(1, $"User {user.Email} added to the {roleName} role");
+               
+                return StatusCode(StatusCodes.Status200OK,
+                           new Response { Status = "Success", Message = $"User {user.Email} added to the {roleName} role" });
+            }
+            else
+            {
+                _logger.LogInformation(1, $"Error: Unable to add user {user.Email} to the {roleName} role");
+                
+                return StatusCode(StatusCodes.Status400BadRequest,
+                            new Response { Status = "Error", Message = $"Error: Unable to add user {user.Email} to the {roleName} role" });
+            }
+        }
+        return BadRequest(new { error = "Unable to find user" });
+    }
+
+    [HttpPost]
     [Route("login")]
     public async Task<IActionResult> Login([FromBody] LoginModel model)
     {
