873be5660756309c3858c7298030cf231386d6c6
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-08-29 10:30:59 -0300

    Testes Unitários : Testando os métodos Delete

---
+++ ApiCatalogoxUnitTests/UnitTests/DeleteProdutoUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using APICatalogo.Controllers;
+using APICatalogo.DTOs;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoxUnitTests.UnitTests;
+
+public class DeleteProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
+{
+    private readonly ProdutosController _controller;
+
+    public DeleteProdutoUnitTests(ProdutosUnitTestController controller)
+    {
+        //NullLogger<ProdutosController>.Instance para enviar um log nulo e rodar os testes sem problemas
+        _controller = new ProdutosController(controller.repository, NullLogger<ProdutosController>.Instance, controller.mapper);
+    }
+
+    //testes para o Delete
+    //testes para o Delete
+    [Fact]
+    public async Task DeleteProdutoById_Return_OkResult()
+    {
+        var prodId = 2;
+
+        // Act
+        var result = await _controller.Delete(prodId) as ActionResult<ProdutoDTO>;
+
+        // Assert
+        result.Should().NotBeNull(); // Verifica se o resultado não é nulo
+        result.Result.Should().BeOfType<OkObjectResult>(); // Verifica se o resultado é OkResult
+    }
+
+    [Fact]
+    public async Task DeleteProdutoById_Return_NotFound()
+    {
+        // Arrange
+        var prodId = 999;
+
+        // Act
+        var result = await _controller.Delete(prodId) as ActionResult<ProdutoDTO>;
+
+        // Assert
+        result.Should().NotBeNull(); // Verifica se o resultado não é nulo
+        result.Result.Should().BeOfType<NotFoundObjectResult>(); // Verifica se o resultado é NotFoundObjectResult
+    }
+}
